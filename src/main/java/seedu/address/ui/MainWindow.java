2b2e8589a39bdefdedd0f9cf177ecfca465b6f18
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-11-13 22:43:16 +0800

    LoginWindow.java: delete file from src/main MainWindow.java: remove call to LoginWindow LoginWindow.fxml: delete file from src/main

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -122,19 +122,6 @@ public class MainWindow extends UiPart<Region> {
         });
     }
 
-
-    //@@author aggarwalRuchir
-    /**
-     * Opens the help window.
-     */
-    @FXML
-    public void handleLogin() {
-        logger.info("Login: Enter username and password to open address book");
-        LoginWindow loginWindow = new LoginWindow();
-        loginWindow.show();
-    }
-    //@@author
-
     /**
      * Fills up all the placeholders of this window.
      */
